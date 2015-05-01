c4f82ac6ca553584279c0040966fbc82ea1e3e14
Matthew D. Huff <matthewhuff89@gmail.com>
2015-05-01 15:55:23 -0400

    Added function to pause all videos after a video has been clicked, and to start them all again after the popup is exited

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ $(document).ready(function() {
     $('video').click(function(e) {
       var source = $('video source').attr("src");
       console.log(source);
-      var hiddenSection = $('section.video_hidden');
-      var hiddenSource = $('section.video_hidden article span video source')
+      var hiddenSource = $('section.video_hidden article span video source');
       hiddenSource.attr("src", source);
+      $("video").each(function(){
+        $(this).get(0).pause();
+      });
+      var hiddenSection = $('section.video_hidden');
       hiddenSection.fadeIn()
         // unhide section.hidden
         .css({ 'display':'block'})
@@ -20,7 +23,10 @@ $(document).ready(function() {
         .appendTo('body');
         })
         $('span.close').click(function() {
-          $(hiddenSection).fadeOut();
+          $('section.video_hidden').fadeOut();
+          $("video").each(function(){
+            $(this).get(0).play();
+          });
         });
     });
 });
