02dcdfd3f82c721144dc2a7cd5596f57cca3b541
Matthew D. Huff <matthewhuff89@gmail.com>
2015-05-08 14:26:56 -0400

    Videos expand into fullscreen upon click, page loads with all videos muted

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
 $(document).ready(function() {
-  $(function(){
-    $('video').click(function(e) {
-      var source = $('video source').attr("src");
-      console.log(source);
-      var hiddenSource = $('section.video_hidden article span video source');
-      hiddenSource.attr("src", source);
-      $("video").each(function(){
-        $(this).get(0).pause();
-      });
-      var hiddenSection = $('section.video_hidden');
-      hiddenSection.fadeIn()
-        // unhide section.hidden
-        .css({ 'display':'block'})
-        // set to full screen
-        .css({ width: $(window).width() + 'px', height: $(window).height() + 'px'})
-        .css({ top:($(window).height() - hiddenSection.height())/
-          2 + 'px',
-          left:($(window).width() - hiddenSection.width())/2 + 'px' })
+  $('video').each(function() {
+    $(this).get(0).defaultMuted = true;
+  });
 
-        // greyed out background
-        .css({'background-color':'rgba(0,0,0,0.5)'})
-        .appendTo('body');
-        })
-        $('span.closing').click(function() {
-          $('section.video_hidden').fadeOut();
-          $("video").each(function(){
-            $(this).get(0).play();
-          });
-        });
-    });
+    $('video').click(function(e) {
+      var $target = $(event.target)
+      $target[0].webkitRequestFullscreen();
+      console.log($target[0].muted);
+      $target.load()
+      $target[0].muted = false;
+
+      //     $("video").each(function(){
+      //       $(this).get(0).play();
+      //     });
+  });
 });
