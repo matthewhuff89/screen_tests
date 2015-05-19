52f3b39ff5875634c368bbad32981f3fb6f4e1f5
Matthew D. Huff <matthewhuff89@gmail.com>
2015-05-19 11:12:47 -0400

    Minor change

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Mess with switching "element" with document in this part...
+// A lot of refactoring needs to be done here.
 function toggleFullScreen(element) {
   if(!element.fullscreenElement && !element.mozFullScreenElement && !element.webkitFullscreenElement && !element.msFullscreenElement ) {
     if(element.requestFullscreen) {
@@ -33,11 +35,14 @@ function toggleFullScreen(element) {
 
 
 $(document).ready(function() {
+
+  // Upon the page being loaded, all of the videos are muted
   $('video').each(function() {
-    var vid = $(this).get(0)
-    vid.defaultMuted = true;
+    $(this).get(0).defaultMuted = true;
   });
 
+
+  // On click, the targeted video is set to full screen mode. Depending on what type of browser is being used, a certain Fullscreen call is made. If the video is currently in fullscreen video, then on click, the Fullscreen mode will be cancelled and the targeted video will once again be muted.
     $('video').click(function(e) {
       var $target = $(event.target)
       if(!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
