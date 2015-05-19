7831c8ac7ae236044b7d20f73370fffda42f9ddf
Matthew D. Huff <matthewhuff89@gmail.com>
2015-05-19 13:16:39 -0400

    Refactored - Took out a lot of if/else statements from the document.ready block

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
 // Mess with switching "element" with document in this part...
 // A lot of refactoring needs to be done here.
+
+var notFullScreen = !document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement
+
 function toggleFullScreen(element) {
-  if(!element.fullscreenElement && !element.mozFullScreenElement && !element.webkitFullscreenElement && !element.msFullscreenElement ) {
-    if(element.requestFullscreen) {
-      element.requestFullscreen();
-      element.load();
-      element.muted = false;
-    } else if (element.msRequestFullscreen) {
-      element.msRequestFullscreen();
-      element.load();
-      element.muted = false;
-    } else if (element.mozRequestFullScreen) {
-      element.mozRequestFullScreen();
-      element.load();
-      element.muted = false;
-    } else if (element.webkitRequestFullscreen){
-      element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-      element.load();
-      element.muted = false;
-    }
-  } else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      console.log("exiting fullscreen like a boss!");
-      document.webkitExitFullscreen();
+  if(notFullScreen) {
+    if(!element.fullscreenElement && !element.mozFullScreenElement && !element.webkitFullscreenElement && !element.msFullscreenElement ) {
+      if(element.requestFullscreen) {
+        element.requestFullscreen();
+        element.load();
+        element.muted = false;
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+        element.load();
+        element.muted = false;
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+        element.load();
+        element.muted = false;
+      } else if (element.webkitRequestFullscreen){
+        element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        element.load();
+        element.muted = false;
+      }
     }
   }
+     else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+        element.muted = true;
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+        element.muted = true;
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+        element.muted = true;
+      } else if (document.webkitExitFullscreen) {
+        console.log("exiting fullscreen like a boss!");
+        document.webkitExitFullscreen();
+        element.muted = true;
+      }
+    }
 }
 
 function muteVideos() {
@@ -55,26 +65,7 @@ $(document).ready(function() {
   // On click, the targeted video is set to full screen mode. Depending on what type of browser is being used, a certain Fullscreen call is made. If the video is currently in fullscreen video, then on click, the Fullscreen mode will be cancelled and the targeted video will once again be muted.
     $('video').click(function(e) {
       var $target = $(event.target)
-      if(!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
       toggleFullScreen($target[0]);
-      } else {
-        if (document.exitFullscreen) {
-           document.exitFullscreen();
-           $target[0].muted = true;
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-            $target[0].muted = true;
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-            $target[0].muted = true;
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-            $target[0].muted = true;
-        }
-      }
-      //     $("video").each(function(){
-      //       $(this).get(0).play();
-      //     });
   });
 });
 
