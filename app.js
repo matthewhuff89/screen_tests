b517b85474080408f460eb64de746bf5dc39fc3a
Matthew D. Huff <matthewhuff89@gmail.com>
2015-05-19 13:02:55 -0400

    Fixed page so that videos will mute themselves whenever they exit fullscreen mode (whether by click, ESC, or button on the video controls

--- app.js
+++ app.js
@@ -33,15 +33,25 @@ function toggleFullScreen(element) {
   }
 }
 
+function muteVideos() {
+  console.log("Muted everything!");
+  var $target = $(event.target)
+  $target[0].muted = true;
+  $('video').each(function() {
+    $(this).get(0).defaultMuted = true;
+  });
+};
 
 $(document).ready(function() {
 
   // Upon the page being loaded, all of the videos are muted
-  $('video').each(function() {
-    $(this).get(0).defaultMuted = true;
+ muteVideos();
+
+  $(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(){
+    if(!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+      muteVideos();
+    };
   });
-
-
   // On click, the targeted video is set to full screen mode. Depending on what type of browser is being used, a certain Fullscreen call is made. If the video is currently in fullscreen video, then on click, the Fullscreen mode will be cancelled and the targeted video will once again be muted.
     $('video').click(function(e) {
       var $target = $(event.target)
