274cc0c166043666ba181f6b44124adf9415d702
Matthew D. Huff <matthewhuff89@gmail.com>
2015-06-13 15:47:34 -0400

    Changed index file

--- app.js
+++ app.js
@@ -60,8 +60,14 @@ $(document).ready(function() {
     };
   });
   // On click, the targeted video is set to full screen mode. Depending on what type of browser is being used, a certain Fullscreen call is made. If the video is currently in fullscreen video, then on click, the Fullscreen mode will be cancelled and the targeted video will once again be muted.
-    $('video').click(function(event) {
-      var $target = $(event.target)
-      toggleFullScreen($target[0]);
+  $('img').click(function(event) {
+    var $target = $(event.target)
+    var $video = $('video').get(0)
+    toggleFullScreen($video);
   });
+
+  $('video').click(function(event) {
+    var $target = $(event.target);
+    toggleFullScreen($target);
+  })
 });
